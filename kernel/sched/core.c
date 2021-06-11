68b4c72c09a5ad7fd6a6ba9353fbf9556a45ff99
Peter Zijlstra <peterz@infradead.org>
2021-06-11 10:28:12 +0200

    sched: Introduce task_is_running()
    Replace a bunch of 'p->state == TASK_RUNNING' with a new helper:
    task_is_running(p).
    
    Change-Id: Idaf8e7c3c67229befe2c8cb00c45936711670dbe
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Davidlohr Bueso <dave@stgolabs.net>
    Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Acked-by: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/20210611082838.222401495@infradead.org

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6192,7 +6192,7 @@ again:
 	if (curr->sched_class != p->sched_class)
 		goto out_unlock;
 
-	if (task_running(p_rq, p) || p->state)
+	if (task_running(p_rq, p) || !task_is_running(p))
 		goto out_unlock;
 
 	yielded = curr->sched_class->yield_to_task(rq, p, preempt);
@@ -6396,7 +6396,7 @@ void sched_show_task(struct task_struct *p)
 
 	printk(KERN_INFO "%-15.15s %c", p->comm, task_state_to_char(p));
 
-	if (p->state == TASK_RUNNING)
+	if (task_is_running(p))
 		printk(KERN_CONT "  running task    ");
 #ifdef CONFIG_DEBUG_STACK_USAGE
 	free = stack_not_used(p);
