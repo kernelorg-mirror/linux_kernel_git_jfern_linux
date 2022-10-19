aab1d3be9cb043a6eeecf07098385b3c7f2e3c4f
Joel Fernandes <joelaf@google.com>
2022-10-19 01:13:44 +0000

    debug: reorder trace_rcu_callback
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    Change-Id: Ieaaa13fbde7fa8717bcf02a23bdf4d700cc28df2

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -2989,10 +2989,6 @@ __call_rcu_common(struct rcu_head *head, rcu_callback_t func, bool lazy)
 	}
 
 	check_cb_ovld(rdp);
-	if (rcu_nocb_try_bypass(rdp, head, &was_alldone, flags, lazy))
-		return; // Enqueued onto ->nocb_bypass, so just leave.
-	// If no-CBs CPU gets here, rcu_nocb_try_bypass() acquired ->nocb_lock.
-	rcu_segcblist_enqueue(&rdp->cblist, head);
 	if (__is_kvfree_rcu_offset((unsigned long)func))
 		trace_rcu_kvfree_callback(rcu_state.name, head,
 					 (unsigned long)func,
@@ -3000,6 +2996,11 @@ __call_rcu_common(struct rcu_head *head, rcu_callback_t func, bool lazy)
 	else
 		trace_rcu_callback(rcu_state.name, head,
 				   rcu_segcblist_n_cbs(&rdp->cblist));
+
+	if (rcu_nocb_try_bypass(rdp, head, &was_alldone, flags, lazy))
+		return; // Enqueued onto ->nocb_bypass, so just leave.
+	// If no-CBs CPU gets here, rcu_nocb_try_bypass() acquired ->nocb_lock.
+	rcu_segcblist_enqueue(&rdp->cblist, head);
 
 	/* Go handle any RCU core processing required. */
 	if (IS_ENABLED(CONFIG_RCU_NOCB_CPU) &&
