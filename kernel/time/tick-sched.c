d299f208e5100b11224d15f9cb3575b4c8d70338
Joel Fernandes (Google) <joel@joelfernandes.org>
2023-02-16 22:03:02 +0000

    hrtimer: Add a sysctl to lower resolution of timers
    On boot, by default the timers switch from periodic to high res mode. However,
    if highres=false is passed, it switches to 'nohz' mode, which is lower res.
    
    This patch adds a sysctl to do the switch to 'nohz' mode at runtime, and back
    to high res mode as well if requested.
    
    Signed-off-by: Joel Fernandes (Google) <joel@joelfernandes.org>

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -1395,7 +1395,7 @@ static inline void tick_nohz_activate(struct tick_sched *ts, int mode)
 /**
  * tick_nohz_switch_to_nohz - switch to nohz mode
  */
-static void tick_nohz_switch_to_nohz(void)
+void tick_nohz_switch_to_nohz(void)
 {
 	struct tick_sched *ts = this_cpu_ptr(&tick_cpu_sched);
 	ktime_t next;
