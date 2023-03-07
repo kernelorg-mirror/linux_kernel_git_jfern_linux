2146b76039f9562bb9e2415662da4cb68dc7e372
Valentin Schneider <vschneid@redhat.com>
2023-03-07 14:35:55 +0000

    irq_work: Trace self-IPIs sent via arch_irq_work_raise()
    IPIs sent to remote CPUs via irq_work_queue_on() are now covered by
    trace_ipi_send_cpumask(), add another instance of the tracepoint to cover
    self-IPIs.
    
    Signed-off-by: Valentin Schneider <vschneid@redhat.com>
    Reviewed-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- kernel/irq_work.c
+++ kernel/irq_work.c
@@ -21,6 +21,8 @@
 #include <asm/processor.h>
 #include <linux/kasan.h>
 
+#include <trace/events/ipi.h>
+
 static DEFINE_PER_CPU(struct llist_head, raised_list);
 static DEFINE_PER_CPU(struct llist_head, lazy_list);
 
@@ -49,6 +51,16 @@ void __weak arch_irq_work_raise(void)
 	 */
 }
 
+static __always_inline void irq_work_raise(struct irq_work *work)
+{
+	if (trace_ipi_send_cpumask_enabled() && arch_irq_work_has_interrupt())
+		trace_ipi_send_cpumask(cpumask_of(smp_processor_id()),
+				       _RET_IP_,
+				       work->func);
+
+	arch_irq_work_raise();
+}
+
 /* Enqueue on current CPU, work must already be claimed and preempt disabled */
 static void __irq_work_queue_local(struct irq_work *work)
 {
@@ -56,10 +68,10 @@ static void __irq_work_queue_local(struct irq_work *work)
 	if (atomic_read(&work->node.a_flags) & IRQ_WORK_LAZY) {
 		if (llist_add(&work->node.llist, this_cpu_ptr(&lazy_list)) &&
 		    tick_nohz_tick_stopped())
-			arch_irq_work_raise();
+			irq_work_raise(work);
 	} else {
 		if (llist_add(&work->node.llist, this_cpu_ptr(&raised_list)))
-			arch_irq_work_raise();
+			irq_work_raise(work);
 	}
 }
 
