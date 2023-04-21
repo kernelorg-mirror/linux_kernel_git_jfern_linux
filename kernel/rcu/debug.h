2df956d971601d26c6568dd888fa0398f20e29d1
Joel Fernandes (Google) <joel@joelfernandes.org>
2023-04-21 00:27:41 +0000

    rcu/debug: Add debugfs nodes to read data
    Signed-off-by: Joel Fernandes (Google) <joel@joelfernandes.org>

--- kernel/rcu/debug.h
+++ kernel/rcu/debug.h
@@ -11,6 +11,8 @@
 #include "rcu.h"
 #include "tree.h"
 
+#include <linux/debugfs.h>
+
 #ifdef CONFIG_RCU_DEBUGFS
 // rdp lock must be held.
 static void __find_func(struct rcu_data *rdp, unsigned long ip, int *B, int *E,
@@ -82,8 +84,60 @@ int rcu_debug_cb_done(struct rcu_data *rdp, void* ip_ptr, struct rcu_head_debug_
 	return 0;
 }
 
-void rcu_debug_init(void)
+DECLARE_PER_CPU_SHARED_ALIGNED(struct rcu_data, rcu_data);
+
+static int rcu_debug_show(struct seq_file *m, void *v)
 {
+	struct rcu_data *rdp;
+	int i, cpu;
+	unsigned long flags;
+
+	for_each_possible_cpu(cpu) {
+		rdp = per_cpu_ptr(&rcu_data, cpu);
+		rcu_nocb_lock_irqsave(rdp, flags);
+		seq_printf(m, "cpu: %d, rcu_debug_ptrs_nr: %d\n", cpu, rdp->rcu_debug_ptrs_nr);
+
+		for (i = 0; i < rdp->rcu_debug_ptrs_nr; i++) {
+			if (!rdp->rcu_debug_ptrs[i].valid)
+				continue;
+			seq_printf(m, "ip: %ps, max_wait in ms: %llu, total_execs: %d\n",
+					(void *)rdp->rcu_debug_ptrs[i].ip,
+					rdp->rcu_debug_ptrs[i].max_wait_ns / 1000000,
+					rdp->rcu_debug_ptrs[i].total_execs);
+		}
+		rcu_nocb_unlock_irqrestore(rdp, flags);
+	}
+	return 0;
+}
+
+static int rcu_debug_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, rcu_debug_show, inode->i_private);
+}
+
+static const struct file_operations rcu_debug_fops = {
+	.owner = THIS_MODULE,
+	.open = rcu_debug_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
+void __init rcu_debug_init(void)
+{
+	struct dentry *rcu_dir, *rcu_file;
+
+	if (WARN_ON_ONCE(sizeof(rcu_head_debug_data) > CONFIG_DEBUG_OBJECTS_RCU_HEAD_SIZE))
+		return;
+
+	rcu_dir = debugfs_create_dir("rcu", NULL);
+	if (!rcu_dir || IS_ERR(rcu_dir))
+		return;
+
+	rcu_file = debugfs_create_file("callbacks", 0444, rcu_dir, NULL,
+			&rcu_debug_fops);
+	if (!rcu_file)
+		debugfs_remove(rcu_dir);
 }
 #else
 int rcu_debug_cb_done(struct rcu_data *rdp, void* ip_ptr, struct rcu_head_debug_data *data)
