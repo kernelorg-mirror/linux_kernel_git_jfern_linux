48a9728e73f960ec6b51cafe42a5a83f0bead876
Joel Fernandes (Google) <joel@joelfernandes.org>
2023-05-06 00:40:00 +0000

    time: Let stop code round to next tick using hrtimer_forward()
    TODO: Check for corner cases where ts->time_expires is no longer
    rounded. Perhaps in the nohz_stop code.
    
    This prevents a case where the next tick is rounded to TICK_NSEC,
    however the tick-sched timer is actually at a non-multiple of TICK_NSEC
    value. This causes timer interrupts in quick succession when the tick is
    stopped and restarted.
    
    Signed-off-by: Joel Fernandes (Google) <joel@joelfernandes.org>

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -954,8 +954,8 @@ static void tick_nohz_stop_tick(struct tick_sched *ts, int cpu)
 		trace_tick_event_program(TPS("NextEventFromStopTick"),
 								 tick, hrtimer_get_expires(&ts->sched_timer), 1,
 								 TPS("lowres"));
-		hrtimer_set_expires(&ts->sched_timer, tick);
-		tick_program_event(tick, 1);
+		hrtimer_forward(&ts->sched_timer, tick, TICK_NSEC);
+		tick_program_event(hrtimer_get_expires(&ts->sched_timer), 1);
 	}
 }
 
