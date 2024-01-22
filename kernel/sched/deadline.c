216567846d0133bc52b1308175f65c4df4f1c4ea
Suleiman Souhlal <suleiman@google.com>
2024-01-22 20:44:14 +0900

    sched: server: Don't start hrtick for DL server tasks
    Otherwise, we might start it even for tasks in a sched class that should
    have it off.
    
    Signed-off-by: Suleiman Souhlal <suleiman@google.com>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -2325,11 +2325,12 @@ static struct task_struct *pick_next_task_dl(struct rq *rq)
 	if (!p)
 		return p;
 
-	if (!p->dl_server)
+	if (!p->dl_server) {
 		set_next_task_dl(rq, p, true);
 
-	if (hrtick_enabled(rq))
-		start_hrtick_dl(rq, &p->dl);
+		if (hrtick_enabled(rq))
+			start_hrtick_dl(rq, &p->dl);
+	}
 
 	return p;
 }
