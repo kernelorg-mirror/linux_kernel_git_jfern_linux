4c303ba75918b6ebc0a77918ad815109fa1785c9
Ben Skeggs <bskeggs@nvidia.com>
2024-05-31 04:55:19 +1000

    drm/nouveau/nvkm: move pci probe() defer from drm
    Signed-off-by: Ben Skeggs <bskeggs@nvidia.com>

--- drivers/gpu/drm/nouveau/nouveau_drm.c
+++ drivers/gpu/drm/nouveau/nouveau_drm.c
@@ -791,9 +791,6 @@ static int nouveau_drm_probe(struct pci_dev *pdev,
 	struct nouveau_drm *drm;
 	int ret;
 
-	if (vga_switcheroo_client_probe_defer(pdev))
-		return -EPROBE_DEFER;
-
 	/* We need to check that the chipset is supported before booting
 	 * fbdev off the hardware, as there's no way to put it back.
 	 */
