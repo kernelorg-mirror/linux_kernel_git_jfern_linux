dc28c452dbfd3b38b2d87d5541bb95e0dc151cf1
Dave Airlie <airlied@redhat.com>
2024-11-20 06:34:06 +1000

    rust/error: add ETIME
    this will be used in Nova to denote timeouts

--- rust/kernel/error.rs
+++ rust/kernel/error.rs
@@ -83,6 +83,7 @@ pub mod code {
     declare_err!(EIOCBQUEUED, "iocb queued, will get completion event.");
     declare_err!(ERECALLCONFLICT, "Conflict with recalled state.");
     declare_err!(ENOGRACE, "NFS file lock reclaim refused.");
+    declare_err!(ETIME, "Timer Expired.");
 }
 
 /// Generic integer kernel error.
