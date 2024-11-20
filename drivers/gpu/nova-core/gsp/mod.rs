70a406a91b283fae905536b69f038ae1f3663513
Dave Airlie <airlied@redhat.com>
2024-11-20 11:52:50 +1000

    nova-core/gsp: add sysmem flush page support
    This allocates a page to act as the system memory flush page.

--- drivers/gpu/nova-core/gsp/mod.rs
+++ drivers/gpu/nova-core/gsp/mod.rs
@@ -4,7 +4,10 @@ pub(crate) use kernel::macros::versions;
 use kernel::prelude::*;
 use kernel::sync::Arc;
 
+use crate::chipsets_before;
 use crate::devinit;
+use crate::dma::DmaObject;
+use crate::gpu::Chipset;
 use crate::gpu::Firmware;
 use crate::gpu::GpuBase;
 
@@ -15,6 +18,7 @@ mod fwsec;
 #[versions(GSP)]
 pub(crate) struct GspManager {
     gpu_base: Arc<GpuBase>,
+    sysmem_flush: DmaObject,
     fw: Firmware,
 }
 
@@ -35,9 +39,21 @@ impl GspManager::ver {
         let vga_base = devinit::vga_workspace_addr(&gpu_base, fb_size, display_disabled)?;
         let _vga_size = fb_size - vga_base;
 
+        let sysmem_flush = DmaObject::new_cleared(&gpu_base.dev, 0x1000, "sysmem flush page")?;
+
+        let bar = gpu_base.bar.try_access().ok_or(ENXIO)?;
+
+        if chipsets_before!(&gpu_base.spec.chipset, GA102) {
+            bar.writel((sysmem_flush.dma.dma_handle() >> 8) as u32, 0x100c10);
+        } else {
+            bar.writel((sysmem_flush.dma.dma_handle() >> 8) as u32, 0x100c10);
+            bar.writel((sysmem_flush.dma.dma_handle() >> 40) as u32, 0x100c40);
+        }
+
         let mgr = GspManager::ver {
             gpu_base,
             fw,
+            sysmem_flush,
         };
 
         let mgr = Arc::new(mgr, GFP_KERNEL)?;
