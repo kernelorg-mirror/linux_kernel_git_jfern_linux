f4d23e428e2b1d5662f1bb7a2f7d58679e963470
Alexandre Courbot <acourbot@nvidia.com>
2025-04-17 16:54:39 +0900

    add riscv firmware

--- drivers/gpu/nova-core/firmware.rs
+++ drivers/gpu/nova-core/firmware.rs
@@ -8,6 +8,7 @@ use kernel::devres::Devres;
 use kernel::firmware;
 use kernel::prelude::*;
 use kernel::str::CString;
+use riscv::RiscvFirmware;
 use sec2::Sec2Firmware;
 
 use crate::dma::DmaObject;
@@ -17,6 +18,7 @@ use crate::gpu;
 use crate::gpu::Chipset;
 
 pub(crate) mod fwsec;
+pub(crate) mod riscv;
 pub(crate) mod sec2;
 
 pub(crate) const FIRMWARE_VERSION: &str = "570.133.07";
@@ -26,7 +28,7 @@ pub(crate) const FIRMWARE_VERSION: &str = "570.133.07";
 pub(crate) struct Firmware {
     pub booter_load: Sec2Firmware,
     pub booter_unload: Sec2Firmware,
-    pub bootloader: firmware::Firmware,
+    pub bootloader: RiscvFirmware,
     pub gsp: firmware::Firmware,
 }
 
@@ -51,7 +53,7 @@ impl Firmware {
                 .and_then(|fw| Sec2Firmware::new(sec2, dev, bar, &fw))?,
             booter_unload: request("booter_unload")
                 .and_then(|fw| Sec2Firmware::new(sec2, dev, bar, &fw))?,
-            bootloader: request("bootloader")?,
+            bootloader: request("bootloader").and_then(|fw| RiscvFirmware::new(dev, &fw))?,
             gsp: request("gsp")?,
         })
     }
@@ -179,6 +181,26 @@ struct HsLoadHeaderV2App {
     pub len: u32,
 }
 impl_from_bytes!(HsLoadHeaderV2App);
+
+#[repr(C)]
+#[derive(Debug)]
+struct RmRiscvUCodeDesc {
+    version: u32,
+    bootloader_offset: u32,
+    bootloader_size: u32,
+    bootloader_param_offset: u32,
+    bootloader_param_size: u32,
+    riscv_elf_offset: u32,
+    riscv_elf_size: u32,
+    app_version: u32,
+    manifest_offset: u32,
+    manifest_size: u32,
+    monitor_data_offset: u32,
+    monitor_data_size: u32,
+    monitor_code_offset: u32,
+    monitor_code_size: u32,
+}
+impl_from_bytes!(RmRiscvUCodeDesc);
 
 pub(crate) struct ModInfoBuilder<const N: usize>(firmware::ModInfoBuilder<N>);
 
