7355b779dde8dedfb79b9040735cd90463aad3ce
Alistair Popple <apopple@nvidia.com>
2025-05-01 15:14:24 +1000

    [Nova] Generate Rust bindings from OpenRM or Nouveau headers
    Nouveau already includes a cleaned up version of the required OpenRM
    headers in drivers/gpu/drm/nouveau/include/nvrm which we can use to
    generate Rust bindings for GSP data structures.
    
    Alternatively the original OpenRM headers can be used to generate
    Rust bindings from, so provide a Kconfig option to specify where these
    headers are.
    
    A Kconfig option is also provided to limit the symbols for which
    bindings are generated, significantly reducing the size of the generated
    Rust bindings.

---
+++ drivers/gpu/nova-core/nvfw/r570_144_bindings.h
@@ -0,0 +1,7 @@
+#include "bindings_helper.h"
+#include <cpuopsys.h>
+#include <vgpu/rpc_global_enums.h>
+#include <gpu/gsp/gsp_static_config.h>
+#include <gpu/gsp/gsp_init_args.h>
+#include <gsp/gsp_fw_wpr_meta.h>
+#include <libos_init_args.h>
