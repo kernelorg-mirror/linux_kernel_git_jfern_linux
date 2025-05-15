48f23be5d8e82b084ff90f83b0c353c414fb347a
Joel Fernandes <joelagnelf@nvidia.com>
2025-05-15 10:06:06 -0400

    nova-core: falcon: Move waiting until halted to a helper
    Move the "waiting until halted" functionality into a helper so that we
    can use it in the sequencer, which is a separate sequencer operation.
    
    Signed-off-by: Joel Fernandes <joelagnelf@nvidia.com>

--- drivers/gpu/nova-core/falcon.rs
+++ drivers/gpu/nova-core/falcon.rs
@@ -493,6 +493,18 @@ impl<E: FalconEngine + 'static> Falcon<E> {
         Ok(())
     }
 
+    /// Wait until the falcon CPU is halted.
+    pub(crate) fn wait_till_halted(&self, bar: &Bar0) -> Result<()> {
+        util::wait_on(Duration::from_secs(2), || {
+            let r = regs::NV_PFALCON_FALCON_CPUCTL::read(bar, E::BASE);
+            if r.halted() {
+                Some(())
+            } else {
+                None
+            }
+        })
+    }
+
     /// Start running the loaded firmware.
     ///
     /// `mbox0` and `mbox1` are optional parameters to write into the `MBOX0` and `MBOX1` registers
@@ -526,14 +538,7 @@ impl<E: FalconEngine + 'static> Falcon<E> {
                 .write(bar, E::BASE),
         }
 
-        util::wait_on(Duration::from_secs(2), || {
-            let r = regs::NV_PFALCON_FALCON_CPUCTL::read(bar, E::BASE);
-            if r.halted() {
-                Some(())
-            } else {
-                None
-            }
-        })?;
+        self.wait_till_halted(bar)?;
 
         let (mbox0, mbox1) = (
             regs::NV_PFALCON_FALCON_MAILBOX0::read(bar, E::BASE).value(),
