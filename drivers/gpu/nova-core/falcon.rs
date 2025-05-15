16cf7c97a91effe08492ba1ecaa814b2c0218360
Joel Fernandes <joelagnelf@nvidia.com>
2025-05-15 18:04:40 -0400

    nova-core: falcon: Move dma_reset functionality into helper
    Signed-off-by: Joel Fernandes <joelagnelf@nvidia.com>

--- drivers/gpu/nova-core/falcon.rs
+++ drivers/gpu/nova-core/falcon.rs
@@ -377,6 +377,13 @@ impl<E: FalconEngine + 'static> Falcon<E> {
         })
     }
 
+    /// Reset DMA-related registers.
+    pub(crate) fn dma_reset(&self, bar: &Bar0) -> Result<()> {
+        regs::NV_PFALCON_FBIF_CTL::alter(bar, &E::ID, |v| v.set_allow_phys_no_ctx(true));
+        regs::NV_PFALCON_FALCON_DMACTL::default().write(bar, &E::ID);
+        Ok(())
+    }
+
     /// Wait for memory scrubbing to complete.
     fn reset_wait_mem_scrubbing(&self, bar: &Bar0) -> Result {
         // TIMEOUT: memory scrubbing should complete in less than 20ms.
@@ -527,8 +534,7 @@ impl<E: FalconEngine + 'static> Falcon<E> {
 
     /// Perform a DMA load into `IMEM` and `DMEM` of `fw`, and prepare the falcon to run it.
     pub(crate) fn dma_load<F: FalconFirmware<Target = E>>(&self, bar: &Bar0, fw: &F) -> Result {
-        regs::NV_PFALCON_FBIF_CTL::alter(bar, &E::ID, |v| v.set_allow_phys_no_ctx(true));
-        regs::NV_PFALCON_FALCON_DMACTL::default().write(bar, &E::ID);
+        self.dma_reset(bar);
         regs::NV_PFALCON_FBIF_TRANSCFG::alter(bar, &E::ID, 0, |v| {
             v.set_target(FalconFbifTarget::CoherentSysmem)
                 .set_mem_type(FalconFbifMemType::Physical)
