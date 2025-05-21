b082e5019db7be7466a422026d79c89e1784077a
Joel Fernandes <joelagnelf@nvidia.com>
2025-05-20 23:04:07 -0400

    nova-core: firmware: Provide accessor for app_version
    Will be used by GSP boot and sequencer code.
    
    Signed-off-by: Joel Fernandes <joelagnelf@nvidia.com>

--- drivers/gpu/nova-core/firmware.rs
+++ drivers/gpu/nova-core/firmware.rs
@@ -320,6 +320,12 @@ pub(crate) struct RmRiscvUCodeDesc {
 }
 unsafe impl FromBytes for RmRiscvUCodeDesc {}
 
+impl RmRiscvUCodeDesc {
+    pub(crate) fn app_version(&self) -> u32 {
+        self.app_version
+    }
+}
+
 pub(crate) struct ModInfoBuilder<const N: usize>(firmware::ModInfoBuilder<N>);
 
 impl<const N: usize> ModInfoBuilder<N> {
