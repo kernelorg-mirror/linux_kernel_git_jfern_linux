8f0cc4edc614d10fb8ac5fb0c731831734324c67
Alistair Popple <apopple@nvidia.com>
2025-06-26 12:51:05 +1000

    nova-core: Create initial GspSharedMemObjects
    Conflicts:
    	drivers/gpu/nova-core/gsp.rs
    
    [jhubbard] Fixed incomplete renaming from GspSharedMemObjects to
    GspMemObjects.

--- drivers/gpu/nova-core/gpu.rs
+++ drivers/gpu/nova-core/gpu.rs
@@ -9,6 +9,7 @@ use crate::fb::SysmemFlush;
 use crate::firmware::fwsec::{FwsecCommand, FwsecFirmware};
 use crate::firmware::{Firmware, FIRMWARE_VERSION};
 use crate::gfw;
+use crate::gsp;
 use crate::regs;
 use crate::util;
 use crate::vbios::Vbios;
@@ -308,6 +309,9 @@ impl Gpu {
         let bios = Vbios::new(pdev, bar)?;
 
         Self::run_fwsec_frts(pdev.as_ref(), &gsp_falcon, bar, &bios, &fb_layout)?;
+
+        let libos = gsp::GspMemObjects::new(pdev)?;
+        let _libos_handle = libos.libos_dma_handle();
 
         Ok(pin_init!(Self {
             spec,
