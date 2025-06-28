0a1992c3db92c0ff43347ba87a27d0517492221e
John Hubbard <jhubbard@nvidia.com>
2025-06-28 14:48:34 -0700

    nova-core: clear MBOX0 before waiting for scrubber completion
    Otherwise, you'll never reach Riscv==true, much less GSP's INIT_DONE
    message.
    
    This is how Nouveau does it.
    
    Signed-off-by: John Hubbard <jhubbard@nvidia.com>

--- drivers/gpu/nova-core/falcon.rs
+++ drivers/gpu/nova-core/falcon.rs
@@ -389,7 +389,11 @@ impl<E: FalconEngine + 'static> Falcon<E> {
 
     /// Wait for memory scrubbing to complete.
     fn reset_wait_mem_scrubbing(&self, bar: &Bar0) -> Result {
-        // TIMEOUT: memory scrubbing should complete in less than 20ms.
+        // Clear MAILBOX0 before waiting for memory scrubbing
+        regs::NV_PFALCON_FALCON_MAILBOX0::default()
+            .set_value(0)
+            .write(bar, &E::ID);
+
         util::wait_on(Delta::from_millis(20), || {
             if regs::NV_PFALCON_FALCON_HWCFG2::read(bar, &E::ID).mem_scrubbing_done() {
                 Some(())
