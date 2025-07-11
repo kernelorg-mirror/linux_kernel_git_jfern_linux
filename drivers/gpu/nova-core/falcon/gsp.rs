71b80a1223bc73a1f873ebff54bf2e37f9dcb1a7
Joel Fernandes <joelagnelf@nvidia.com>
2025-07-10 21:41:04 -0400

    nova-core: sequencer: Implement core resume operation
    Implement core resume operation. This is the last step of the sequencer
    resulting in resume of the GSP and proceeding to INIT_DONE stage of GSP
    boot.
    
    Signed-off-by: Joel Fernandes <joelagnelf@nvidia.com>

--- drivers/gpu/nova-core/falcon/gsp.rs
+++ drivers/gpu/nova-core/falcon/gsp.rs
@@ -35,7 +35,6 @@ impl Falcon<Gsp> {
     }
 
     /// Function to check if GSP reload/resume has completed during the boot process.
-    #[expect(dead_code)]
     pub(crate) fn check_reload_completed(
         &self,
         bar: &Bar0,
