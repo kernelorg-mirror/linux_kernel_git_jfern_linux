891a31bdc2efe32779f5d6a2247966de73c1bae0
Christian S. Lima <christiansantoslima21@gmail.com>
2025-08-11 18:38:51 -0300

    rust: transmute: Add methods for FromBytes trait
    The two methods added take a slice of bytes and return those bytes in a
    specific type. These methods are useful when we need to transform the
    stream of bytes into specific type.
    
    The `Frombytessized` trait was added to make it easier to implement other
    user defined types within the codebase. With the current implementation,
    there's no way to interact without implementing `from_bytes` and
    `from_mut_bytes` for every new type, and this would end up generating a lot
    of duplicate code. By using FromBytesSized as a proxy trait, we can avoid
    this without generating a direct dependency. If necessary, the user can
    simply implement `FromBytes` if needed. For more context, please check the
    [1] and [2].
    
    [1] https://lore.kernel.org/rust-for-linux/DANSZ6Q476EC.3GY00K717QVUL@nvidia.com/
    [2] https://lore.kernel.org/rust-for-linux/DAOESYD6F287.3U3M64X0S1WN5@nvidia.com/
    
    Link: https://github.com/Rust-for-Linux/linux/issues/1119
    Suggested-by: Alexandre Courbot <acourbot@nvidia.com>
    Signed-off-by: Christian S. Lima <christiansantoslima21@gmail.com>

--- rust/kernel/transmute.rs
+++ rust/kernel/transmute.rs
@@ -2,6 +2,8 @@
 
 //! Traits for transmuting types.
 
+use crate::build_assert;
+
 /// Types for which any bit pattern is valid.
 ///
 /// Not all types are valid for all values. For example, a `bool` must be either zero or one, so
@@ -9,27 +11,136 @@
 ///
 /// It's okay for the type to have padding, as initializing those bytes has no effect.
 ///
+/// # Examples
+///
+/// ```
+/// use kernel::transmute::FromBytes;
+///
+/// let foo = [1, 2, 3, 4];
+///
+/// let result = u32::from_bytes(&foo).unwrap();
+///
+/// #[cfg(target_endian = "little")]
+/// assert_eq!(*result, 0x4030201);
+///
+/// #[cfg(target_endian = "big")]
+/// assert_eq!(*result, 0x1020304);
+/// ```
+///
 /// # Safety
 ///
 /// All bit-patterns must be valid for this type. This type must not have interior mutability.
-pub unsafe trait FromBytes {}
+pub unsafe trait FromBytes {
+    /// Converts a slice of bytes to a reference to `Self` when the reference
+    /// is properly aligned and the size of slice is equal to that of `T`
+    /// and is different from zero. In another case, it will return
+    ///`None`.
+    fn from_bytes(bytes: &[u8]) -> Option<&Self>;
 
-macro_rules! impl_frombytes {
+    /// Converts a mutable slice of bytes to a reference to `Self`
+    /// when the reference is properly aligned and the size of slice
+    /// is equal to that of `T` and is different from zero. In another
+    /// case, it will return `None`.
+    fn from_bytes_mut(bytes: &mut [u8]) -> Option<&mut Self>
+    where
+        Self: AsBytes;
+}
+
+/// Provide an auto-implementation of FromBytes's methods for all
+/// sized types, if you need an implementation for your type use this instead.
+///
+/// # Safety
+///
+/// All bit-patterns must be valid for this type. This type must not have interior mutability.
+pub unsafe trait FromBytesSized: Sized {}
+
+macro_rules! impl_frombytessized {
     ($($({$($generics:tt)*})? $t:ty, )*) => {
         // SAFETY: Safety comments written in the macro invocation.
-        $(unsafe impl$($($generics)*)? FromBytes for $t {})*
+        $(unsafe impl$($($generics)*)? FromBytesSized for $t {})*
     };
 }
 
-impl_frombytes! {
+impl_frombytessized! {
     // SAFETY: All bit patterns are acceptable values of the types below.
     u8, u16, u32, u64, usize,
     i8, i16, i32, i64, isize,
 
     // SAFETY: If all bit patterns are acceptable for individual values in an array, then all bit
     // patterns are also acceptable for arrays of that type.
-    {<T: FromBytes>} [T],
-    {<T: FromBytes, const N: usize>} [T; N],
+    {<T: FromBytesSized, const N: usize>} [T; N],
+}
+
+// SAFETY: The `FromBytesSized` implementation guarantees that all bit
+// patterns are acceptable values of the types and in array case if
+// all bit patterns are acceptable for individual values in an array,
+// then all bit patterns are also acceptable for arrays of that type.
+unsafe impl<T> FromBytes for T
+where
+    T: FromBytesSized,
+{
+    fn from_bytes(bytes: &[u8]) -> Option<&Self> {
+        let slice_ptr = bytes.as_ptr().cast::<T>();
+        let size = ::core::mem::size_of::<T>();
+        if bytes.len() == size && slice_ptr.is_aligned() {
+            // SAFETY: Since the code checks the size and alignment, the slice is valid.
+            unsafe { Some(&*slice_ptr) }
+        } else {
+            None
+        }
+    }
+
+    fn from_bytes_mut(bytes: &mut [u8]) -> Option<&mut Self>
+    where
+        Self: AsBytes,
+    {
+        let slice_ptr = bytes.as_mut_ptr().cast::<T>();
+        let size = ::core::mem::size_of::<T>();
+        if bytes.len() == size && slice_ptr.is_aligned() {
+            // SAFETY: Since the code checks the size and alignment, the slice is valid.
+            unsafe { Some(&mut *slice_ptr) }
+        } else {
+            None
+        }
+    }
+}
+
+// SAFETY: If all bit patterns are acceptable for individual values in an array, then all bit
+// patterns are also acceptable for arrays of that type.
+unsafe impl<T: FromBytes> FromBytes for [T] {
+    fn from_bytes(bytes: &[u8]) -> Option<&Self> {
+        let size = ::core::mem::size_of::<T>();
+        build_assert!(size == 0, "Can't create a slice with zero elements");
+        let slice_ptr = bytes.as_ptr().cast::<T>();
+        if bytes.len() % size == 0 && slice_ptr.is_aligned() {
+            // SAFETY: Since the number of elements is different from
+            // zero and the pointer is aligned, the slice is valid.
+            unsafe { Some(::core::slice::from_raw_parts(slice_ptr, bytes.len() / size)) }
+        } else {
+            None
+        }
+    }
+
+    fn from_bytes_mut(bytes: &mut [u8]) -> Option<&mut Self>
+    where
+        Self: AsBytes,
+    {
+        let size = ::core::mem::size_of::<T>();
+        build_assert!(size == 0, "Can't create a slice with zero elements");
+        let slice_ptr = bytes.as_mut_ptr().cast::<T>();
+        if bytes.len() % size == 0 && slice_ptr.is_aligned() {
+            // SAFETY: Since the number of elements is different from
+            // zero and the pointer is aligned, the slice is valid.
+            unsafe {
+                Some(::core::slice::from_raw_parts_mut(
+                    slice_ptr,
+                    bytes.len() / size,
+                ))
+            }
+        } else {
+            None
+        }
+    }
 }
 
 /// Types that can be viewed as an immutable slice of initialized bytes.
