ba4e4731525e6ef9e74e67ba0961750cb1e6d501
John Hubbard <jhubbard@nvidia.com>
2025-08-13 17:53:02 -0700

    gpu: nova-core: avoid probing non-display/compute PCI functions
    NovaCore has so far been too imprecise about figuring out if .probe()
    has found a supported PCI PF (Physical Function). By that I mean:
    .probe() sets up BAR0 (which involves a lot of very careful devres and
    Device<Bound> details behind the scenes). And then if it is dealing with
    a non-supported device such as the .1 audio PF on many GPUs, it fails
    out due to an unexpected BAR0 size. We have been fortunate that the BAR0
    sizes are different.
    
    Really, we should be filtering on PCI class ID instead. These days I
    think we can confidently pick out Nova's supported PF's via PCI class
    ID. And if not, then we'll revisit.
    
    The approach here is to filter on "Display VGA" or "Display 3D", which
    is how PCI class IDs express "this is a modern GPU's PF".
    
    Cc: Danilo Krummrich <dakr@kernel.org>
    Cc: Alexandre Courbot <acourbot@nvidia.com>
    Signed-off-by: John Hubbard <jhubbard@nvidia.com>

--- drivers/gpu/nova-core/driver.rs
+++ drivers/gpu/nova-core/driver.rs
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0
 
 use kernel::{
-    auxiliary, bindings, c_str, device::Core, dma::Device, dma::DmaMask, pci, prelude::*,
-    sizes::SZ_16M, sync::Arc,
+    auxiliary, bindings, c_str, device::Core, dma::Device, dma::DmaMask, pci, pci::Class,
+    prelude::*, sizes::SZ_16M, sync::Arc,
 };
 
 use crate::gpu::Gpu;
@@ -21,10 +21,25 @@ kernel::pci_device_table!(
     PCI_TABLE,
     MODULE_PCI_TABLE,
     <NovaCore as pci::Driver>::IdInfo,
-    [(
-        pci::DeviceId::from_id(bindings::PCI_VENDOR_ID_NVIDIA, bindings::PCI_ANY_ID as u32),
-        ()
-    )]
+    [
+        // Modern NVIDIA GPUs will show up as either VGA or 3D controllers.
+        (
+            pci::DeviceId::from_class_and_vendor(
+                Class::DISPLAY_VGA,
+                Class::MASK_CLASS_SUBCLASS,
+                bindings::PCI_VENDOR_ID_NVIDIA
+            ),
+            ()
+        ),
+        (
+            pci::DeviceId::from_class_and_vendor(
+                Class::DISPLAY_3D,
+                Class::MASK_CLASS_SUBCLASS,
+                bindings::PCI_VENDOR_ID_NVIDIA
+            ),
+            ()
+        ),
+    ]
 );
 
 impl pci::Driver for NovaCore {
